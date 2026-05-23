1af634188d5353ac4174d8d91705089069bdcb3a
Sandesh Shrestha <081bei027@ioepc.edu.np>
2026-05-23 17:41:28 +0545

    initial commit

---
+++ 1.py
@@ -0,0 +1,55 @@
+name="sandesh"
+print(f"name")
+
+name='sandesh'
+age=25
+height=5.6
+is_student=True
+print(name,'is',age,'years old')
+print(type(height))
+
+score=90
+if score>=90:
+    print('Grade:A')
+elif score >=70:
+    print('Grade: B')
+else:
+    print("Grade :F")
+
+fruits=['apple','banana','cherry','sandesh']
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)    
+
+count=0
+while count<3:
+    print('count:',count)    
+    count+=1
+
+nums=[3,2,5,1,7,90]
+nums.sort()
+print(nums)
+
+student={
+    'name':'sandesh',
+    'age':25,
+    'is_student':True
+}
+print(student["name"])
+print(student["age"])
+print(student["is_student"])
+
+student['height']=5.6
+print(student['height'])
+student['age']=90
+print(student)
+
+for key, value in student.items():
+    print(key, value)
+print(student.keys())
+print(student.values())
+print(student.items())
+
+
